4bd71148efe7bebbd4c8d700c1b9c34bdf10ea8b
sentotgalihwibowo <54528744+sentotgalihwibowo@users.noreply.github.com>
2021-01-21 22:38:17 +0700

    update pipeline

--- src/com/workshop/Config.groovy
+++ src/com/workshop/Config.groovy
@@ -1,6 +1,10 @@
 #!/usr/bin/groovy
 package com.workshop
-
+ 
 class Config {
-    // Place Default Variable Here
+   // Docker related default variable
+   def default_docker_jenkins_tool = 'docker'
+ 
+   // Golang related default variable
+   def default_golang_base_image = "tobapramudia/tkpd-demo:onbuild"
 }
